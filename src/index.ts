a96f64f2448e0b551a116e9efd05d9b2b734f81a
Sourodeep Chatterjee <sourodeep224@outlook.com>
2022-01-23 21:07:22 +0530

    Added directory structure

--- src/index.ts
+++ src/index.ts
@@ -8,33 +8,36 @@ import write, { writeScore } from "./writer";
 
 const INPUT_DIRECTORY = "inputs";
 const OUTPUT_DIRECTORY = "solutions";
-const SCORES_DIRECTORY = "best_scores";
+const SCORES_DIRECTORY = "bestScores";
 const INPUTS_PATH = path.join(__dirname, "..", INPUT_DIRECTORY);
 
 //reads every input case, parses the content and computes the solution and the score. If the score is better than the best cached score, saves the output to file
 fs.readdir(INPUTS_PATH, (err, files) => {
 	if (err) return console.error(err);
 	files.forEach((file) => {
-		const input_path = path.join(INPUTS_PATH, file);
-		const output_path = input_path
+		const inputPath = path.join(INPUTS_PATH, file);
+		const outputPath = inputPath
 			.replace(INPUT_DIRECTORY, OUTPUT_DIRECTORY)
 			.replace(".in", ".out");
-		const score_path = input_path
+		const scorePath = inputPath
 			.replace(INPUT_DIRECTORY, SCORES_DIRECTORY)
 			.replace(".in", ".score");
-		const data = parse(input_path);
+
+		const data = parse(inputPath);
 		const solution = solve(data);
 		const points = score(data, solution);
-		const best_score = readScore(score_path);
+		const bestScore = readScore(scorePath);
+
 		console.log(
 			`Case ${file}: solution score`,
 			points,
 			"best score",
-			best_score
+			bestScore
 		);
-		if (points > best_score) {
-			write(solution, output_path);
-			writeScore(points, score_path);
+
+		if (points > bestScore) {
+			write(solution, outputPath);
+			writeScore(points, scorePath);
 		}
 	});
 });
